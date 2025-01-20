c2aa175339749954cf5077d33612e1598fd5ec66
tripleRRR33 <florianroy555@gmail.com>
2025-01-20 12:12:56 +0100

    Update pop.js

--- pop.js
+++ pop.js
@@ -5,6 +5,96 @@ const questions = [
     correct: 2,
     anecdote: "Bien qu’il ait été dépassé par Avatar en termes de recettes brutes, Autant en emporte le vent reste le plus rentable si l’on ajuste les recettes à l’inflation."
 },
+   {
+    question: "Quel est le groupe légendaire qui a écrit Bohemian Rhapsody ?",
+    options: ["The Beatles", "Queen", "Pink Floyd", "Led Zeppelin"],
+    correct: 1,
+    anecdote: "Cette chanson, écrite par Freddie Mercury, est l'une des compositions les plus célèbres et originales du groupe, connue pour sa structure complexe et ses influences variées."
+},
+{
+    question: "En quelle année Michael Jackson a-t-il sorti son album Thriller ?",
+    options: ["1979", "1982", "1984", "1987"],
+    correct: 1,
+    anecdote: "Thriller reste l'album le plus vendu de tous les temps, avec des tubes comme 'Billie Jean' et 'Beat It', et a solidifié Michael Jackson en tant que 'King of Pop'."
+},
+{
+    question: "Quel artiste a popularisé la danse du 'moonwalk' ?",
+    options: ["James Brown", "Elvis Presley", "Michael Jackson", "Prince"],
+    correct: 2,
+    anecdote: "Le moonwalk est devenu emblématique après la performance de Jackson lors du concert de Motown 25: Yesterday, Today, Forever en 1983."
+},
+{
+    question: "Quelle chanson de Nirvana a été un hymne de la scène grunge dans les années 1990 ?",
+    options: ["Come As You Are", "Lithium", "Smells Like Teen Spirit", "Heart-Shaped Box"],
+    correct: 2,
+    anecdote: "Cette chanson, souvent vue comme l'hymne de la génération X, a marqué l'essor du mouvement grunge et propulsé Nirvana sur la scène internationale."
+},
+{
+    question: "Qui a chanté 'Like a Virgin', un des plus grands succès des années 1980 ?",
+    options: ["Cyndi Lauper", "Whitney Houston", "Madonna", "Cher"],
+    correct: 2,
+    anecdote: "Madonna a redéfini la pop dans les années 80 avec des chansons comme 'Like a Virgin', et elle est devenue l'une des artistes les plus influentes de l'histoire de la musique."
+},
+{
+    question: "Quel est le jeu vidéo le plus vendu de tous les temps ?",
+    options: ["Grand Theft Auto V", "Minecraft", "Tetris", "Fortnite"],
+    correct: 1,
+    anecdote: "Minecraft a surpassé les autres jeux avec plus de 200 millions d'exemplaires vendus, offrant une expérience de monde ouvert et de création sans fin."
+},
+{
+    question: "Quelle franchise de jeu vidéo est devenue célèbre grâce à des personnages comme Mario et Luigi ?",
+    options: ["Super Mario", "Donkey Kong", "Sonic the Hedgehog", "The Legend of Zelda"],
+    correct: 0,
+    anecdote: "Créée par Nintendo, Super Mario a lancé une série de jeux qui a marqué l’histoire du jeu vidéo, devenant un des personnages les plus iconiques du secteur."
+},
+{
+    question: "Dans quelle série de jeux vidéo le personnage de Link doit-il sauver la princesse Zelda ?",
+    options: ["Final Fantasy", "The Legend of Zelda", "Dragon Quest", "Elder Scrolls"],
+    correct: 1,
+    anecdote: "Cette série, lancée par Nintendo en 1986, combine aventure, exploration et résolution de puzzles, avec Link combattant des forces maléfiques pour sauver Hyrule."
+},
+{
+    question: "Quel jeu vidéo a popularisé le mode Battle Royale en 2017 ?",
+    options: ["PlayerUnknown's Battlegrounds", "Fortnite", "Call of Duty: Warzone", "Apex Legends"],
+    correct: 1,
+    anecdote: "Fortnite a révolutionné les jeux en ligne en introduisant un mode Battle Royale gratuit, avec des compétitions mondiales et un immense succès commercial."
+},
+{
+    question: "Quel jeu a marqué la fin d'une génération avec sa sortie en 1998, mettant en vedette un personnage nommé Solid Snake ?",
+    options: ["Metal Gear Solid", "Resident Evil 2", "Final Fantasy VII", "Half-Life"],
+    correct: 0,
+    anecdote: "Ce jeu, réalisé par Hideo Kojima, est devenu un classique du genre action-infiltration et a marqué l'histoire des consoles PlayStation."
+},
+{
+    question: "Quelle série télévisée a popularisé l’expression 'Winter is Coming' ?",
+    options: ["Breaking Bad", "Vikings", "Game of Thrones", "The Witcher"],
+    correct: 2,
+    anecdote: "Game of Thrones, adaptée des livres de George R. R. Martin, a captivé les téléspectateurs avec ses intrigues complexes, ses personnages profonds et ses moments dramatiques."
+},
+{
+    question: "Quelle série télévisée a réuni des personnages comme Walter White et Jesse Pinkman ?",
+    options: ["The Wire", "Sons of Anarchy", "Breaking Bad", "Better Call Saul"],
+    correct: 2,
+    anecdote: "Cette série acclamée suit l’évolution de Walter White, un professeur de chimie devenu fabricant de méthamphétamine, et de son acolyte Jesse."
+},
+{
+    question: "Quelle série a introduit le personnage de Don Draper dans les années 1960 ?",
+    options: ["The Sopranos", "Mad Men", "Boardwalk Empire", "The Americans"],
+    correct: 1,
+    anecdote: "Mad Men explore la vie de publicitaires dans les années 60, avec un focus sur les contradictions sociales, politiques et personnelles de l’époque."
+},
+{
+    question: "Quelle série à succès a introduit des personnages comme Eleven et Mike Wheeler ?",
+    options: ["Stranger Things", "The OA", "Dark", "Black Mirror"],
+    correct: 0,
+    anecdote: "Mélange de science-fiction et de drame psychologique, cette série s’inspire des années 80 et plonge les téléspectateurs dans un monde mystérieux rempli de créatures surnaturelles."
+},
+{
+    question: "Quel personnage de Friends a un travail de scientifique en biologie ?",
+    options: ["Joey Tribbiani", "Ross Geller", "Chandler Bing", "Rachel Green"],
+    correct: 1,
+    anecdote: "Ross, incarné par David Schwimmer, est l'un des six amis principaux de la série, connu pour ses aventures amoureuses et sa carrière de paléontologue."
+},
 {
     question: "Quel film a introduit le personnage de Thanos dans l'univers cinématographique Marvel ?",
     options: ["Iron Man", "Les Gardiens de la Galaxie", "Les Avengers", "Thor"],
